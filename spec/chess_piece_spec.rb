8b5d6b52921249bd65cb1735147e54fb05e9f359
frobee <frobi.webdev@outlook.com>
2023-01-20 13:57:49 +1030

    test-moving-a-pawn-up-one-position

--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -1,18 +1,14 @@
-require_relative "../lib/pieces/board.rb"
+require_relative "../lib/board.rb"
 
 describe "#move_to" do
-    subject(:board) { Board.new }
+    subject(:chess) { Board.new }
     
     
     context "when the move_to method is called on a pawn" do
-        
-        before do
-            board.setup_board
-        end
 
         it "moves the piece to the specified position" do
-            expect(board[5][0]).to eq white_pawn
-            
+            expect(chess.board[5][0].symbol).to eq "♙ "
+            board[6][0].move_to("5 0")
         end
         
     end
