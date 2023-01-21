8758b08267ff131e0a5362980689154b43395cb8
frobee <frobi.webdev@outlook.com>
2023-01-21 19:47:11 +1030

    refactor-tests

--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -2,16 +2,25 @@ require_relative "../lib/board.rb"
 
 describe "#move_to" do
     subject(:chess) { Board.new }
+  
+    let(:piece) { chess.board[6][0] }
     
-    
+    def move_piece
+        piece.move_to("5 0")
+        chess.update_position(piece)
+    end
+  
     context "when the move_to method is called on a pawn" do
+  
+        before { move_piece }
 
         it "moves the piece to the specified position" do
-            expect(chess.board[5][0].symbol).to eq "♙ "
-            board[6][0].move_to("5 0")
+        expect(chess.board[5][0].symbol).to eq "♙ "
         end
-        
-    end
 
-    
+        it "updates the previous location to nil on the board" do
+        expect(chess.board[6][0]).to eq nil
+        end
+
+    end
 end
