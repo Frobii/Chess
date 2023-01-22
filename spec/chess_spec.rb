aba6fb944ff8bb5d3ced7ab9788d55c4ba19317c
frobee <frobi.webdev@outlook.com>
2023-01-22 18:06:51 +1030

    write-tests-for-pawn-rules

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -4,23 +4,29 @@ describe "#move_to" do
     subject(:chess) { Board.new }
   
     let(:piece) { chess.board[6][0] }
-    
-    def move_piece
-        piece.move_to("5 0")
-        chess.update_position(piece)
-    end
   
-    context "when the move_to method is called on a pawn" do
-  
-        before { move_piece }
+    context "when a pawn is moved" do
 
-        it "moves the piece to the specified position" do
-        expect(chess.board[5][0].symbol).to eq "♙ "
+        it "allows the pawn to move forwards one positon" do
+            piece.move_to("5 0")
+            chess.update_position(piece)
+            expect(chess.board[5][0].symbol).to eq "♙ "
         end
 
         it "updates the previous location to nil on the board" do
-        expect(chess.board[6][0]).to eq nil
+            piece.move_to("5 0")
+            chess.update_position(piece)
+            expect(chess.board[6][0]).to eq nil
         end
 
+        it "doesn't allow the pawn to move forward two positions after it's first move" do
+            piece.move_to("6 0")
+            piece.move_to("4 0")
+            chess.update_position(piece)
+            expect(chess.board[4][0]).to eq nil
+        end
+
+
     end
+
 end
