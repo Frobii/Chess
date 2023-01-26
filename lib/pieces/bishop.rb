f53eb844aadd8b9a90464c1748309a27581119f2
frobee <frobi.webdev@outlook.com>
2023-01-26 13:23:27 +1030

    disallow-bishop-movement-along-just-x-or-y

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -3,15 +3,23 @@ require_relative "../piece.rb"
 class Bishop < Piece
     attr_reader :symbol
 
-    def initialize(color, position)
-        super(color, position)
+    def initialize(color, position, old_position = nil)
+        super(color, position, old_position)
         @symbol = "♝ " if color == "b"
         @symbol = "♗ " if color == "w"
     end
 
     def move_to(new_position)
+        super(new_position)
 
-        super
+        x, y = self.position[0].to_i, self.position[1].to_i
+        old_x, old_y = self.old_position[0].to_i, self.old_position[1].to_i
+
+        if x == old_x || y == old_y
+            self.position = self.old_position
+            self.old_position = nil
+        end
+
     end
 
 end
