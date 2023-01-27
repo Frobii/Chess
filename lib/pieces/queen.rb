e6563929afd898ca0600bc07177da3bd50d60ac2
frobee <frobi.webdev@outlook.com>
2023-01-27 11:53:06 +1030

    stop-queens-moving-around-corners

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -15,6 +15,35 @@ class Queen < Piece
         x, y = self.position[0].to_i, self.position[1].to_i
         old_x, old_y = self.old_position[0].to_i, self.old_position[1].to_i
 
+        # modify bishops rules to suit the queens pure x or y travel
+        if x != old_x && y != old_y
+            if old_x > x
+                if old_y > y
+                    if (old_x - x) != (old_y - y)
+                        self.position = self.old_position
+                        self.old_position = nil
+                    end
+                else
+                    if (old_x - x) != (y - old_y)
+                        self.position = self.old_position
+                        self.old_position = nil
+                    end
+                end
+            else
+                if old_y > y
+                    if (x - old_x) != (old_y - y)
+                        self.position = self.old_position
+                        self.old_position = nil
+                    end
+                else
+                    if (x - old_x) != (y - old_y)
+                        self.position = self.old_position
+                        self.old_position = nil
+                    end
+                end
+            end
+        end
+
     end
 
 end
