8f10d34c07bcf4e4daa1d2809ec6aa30df6b26dc
frobee <frobi.webdev@outlook.com>
2023-01-27 11:39:17 +1030

    fix-inheritance-issues

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -3,17 +3,18 @@ require_relative "../piece.rb"
 class Queen < Piece
     attr_reader :symbol
 
-    def initialize(color, position)
-        super(color, position)
+    def initialize(color, position, old_position = nil)
+        super(color, position, old_position)
         @symbol = "♛ " if color == "b"
         @symbol = "♕ " if color == "w"
     end
 
-    def move_to
+    def move_to(new_position)
+        super(new_position)
 
-        
+        x, y = self.position[0].to_i, self.position[1].to_i
+        old_x, old_y = self.old_position[0].to_i, self.old_position[1].to_i
 
-        super
     end
 
 end
