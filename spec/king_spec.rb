8a9f0a5585da88d76ba2844cf941175421aa8982
frobee <frobi.webdev@outlook.com>
2023-01-27 12:43:32 +1030

    test-king-edge-cases

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -41,5 +41,37 @@ describe "#move_to" do
             expect(chess.board[6][4].symbol).to eq "♔ "
         end
 
+        it "can't take a friendly piece" do
+            king.move_to("7 3")
+            chess.update_position(king)
+            expect(chess.board[6][4].symbol).to eq "♔ "
+        end
+
+        let (:enemy_knight) {chess.board[0][6]}
+
+        it "can take an enemy piece" do
+            enemy_knight.move_to("2 5")
+            chess.update_position(enemy_knight)
+            enemy_knight.move_to("4 6")
+            chess.update_position(enemy_knight)
+            enemy_knight.move_to("6 5")
+            chess.update_position(enemy_knight)
+            king.move_to("6 5")
+            chess.update_position(king)
+            expect(chess.board[6][5].symbol).to eq "♔ "
+        end
+
+        let (:enemy_pawn) {chess.board[1][5]}
+
+        it "can't move to a check position" do
+            enemy_pawn.move_to("3 5")
+            chess.update_position(enemy_pawn)
+            king.move_to("5 4")
+            chess.update_position(king)
+            king.move_to("4 4")
+            chess.update_position(king)
+            expect(chess.board[5][4].symbol).to eq "♔ "
+        end
+
     end
 end
